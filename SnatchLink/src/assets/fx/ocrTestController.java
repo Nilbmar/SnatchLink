3ec9e3cbb287b1d4303b228a18ba0e4a00a78bef
Nilbmar <Nilbmar@gmail.com>
2017-09-06 16:42:55 -0400

    Initial setup of JavaFX Controller for ocrTest.fxml

---
+++ SnatchLink/src/assets/fx/ocrTestController.java
@@ -0,0 +1,35 @@
+package assets.fx;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.image.ImageView;
+import javafx.scene.web.WebView;
+
+public class ocrTestController {
+
+	// Menu Items
+	public MenuItem menuOpenImage;
+	
+	// Views
+	public TreeView<String> treeView;
+	public WebView webView;
+	public ImageView imgView;
+	
+	// Buttons
+	public Button btnGetLink;
+	public Button btnOpenLink;
+	public Button btnOpenInBrowser;
+	public Button btnSnipFromWeb;
+	
+	
+	// Call dialog for selecting an image file on the file system
+	public void openImage() {
+		
+	}
+	
+	// Start process for getting a link from an image
+	public void getLink() {
+		
+	}
+}
